75a030563007c7c92ffcdd9ae54ce5792c1c6174
Melanie Bruns <mbruns42@mail.uni-paderborn.de>
2018-03-20 16:14:25 +0100

    Change artifact names and add structure for ant task

---
+++ ant/src/main/java/org/upb/cryptoanalysis/ant/CryptoAnalysis.java
@@ -0,0 +1,12 @@
+package org.upb.cryptoanalysis.ant;
+
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.BuildException;
+
+public class CryptoAnalysis extends Task {
+
+    @Override
+    public void execute() throws BuildException{
+
+    }
+}
