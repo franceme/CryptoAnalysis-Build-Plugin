70ff37942ab76eefbddea8ef5478db1eb0e20834
Melanie Bruns <mbruns42@mail.uni-paderborn.de>
2018-03-22 16:17:27 +0100

    Add class for Issue which can be converted to json using jackson

---
+++ core/src/main/java/org/upb/cryptoanalysis/core/Issue.java
@@ -0,0 +1,41 @@
+package org.upb.cryptoanalysis.core;
+
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Class for a single issue, includes converting the issue to json
+ * //TODO figure out where to check for existing github ids
+ *
+ */
+public class Issue {
+
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    private int id;
+    private String title;
+    /**
+     * Explanation of the problem. Should include the line, so that
+     * multiple occurrences of the same problem can be distinguished.
+     */
+    private String body;
+    private List<String> labels;
+
+    public Issue(String title, String body, List<String> labels) {
+        this.title = title;
+        this.body = body;
+        this.labels = labels;
+    }
+
+    public String getJsonString(){
+        String jsonInString = null;
+        try {
+            jsonInString = mapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return jsonInString;
+    }
+}
