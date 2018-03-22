2287a7d25fc0d129d8a0d733d761e0bce1810221
Melanie Bruns <mbruns42@mail.uni-paderborn.de>
2018-03-22 13:24:18 +0100

    Move mojo annotations and import log from maven

--- maven/src/main/java/org/upb/cryptoanalysis/maven/CryptoAnalysisMojo.java
+++ maven/src/main/java/org/upb/cryptoanalysis/maven/CryptoAnalysisMojo.java
@@ -2,18 +2,19 @@ package org.upb.cryptoanalysis.maven;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Mojo;
 
+/**
+ * Runs the CogniCrypt static analysis to
+ * check used security for potential security problems.
+ *
+ */
+@Mojo( name = "cryptoanalysis")
 public class CryptoAnalysisMojo extends AbstractMojo {
 
     private Log log = getLog();
 
-    /**
-     * Runs the CogniCrypt static analysis to
-     * check used security for potential security problems.
-     *
-     */
-    @Mojo( name = "cryptoanalysis")
     public void execute() throws MojoExecutionException
     {
         log.info("CryptoAnalysis plugin started");
