eb0b33a27da2000dbe85fd599a4d1b4f17774497
Melanie Bruns <mbruns42@mail.uni-paderborn.de>
2018-03-22 14:24:51 +0100

    Add core Analysis class

---
+++ core/src/main/java/org/upb/cryptoanalysis/core/Analysis.java
@@ -0,0 +1,16 @@
+package org.upb.cryptoanalysis.core;
+
+/**
+ * Core analysis class that starts the crypto analysis independent of the build tool or cli.
+ * TODO: Should we make a AnalysisTask so thinks can be parallelized for different jars
+ */
+public class Analysis {
+    /* The settings for the analysis */
+    private Settings settings;
+
+    public Analysis(Settings settings){
+
+    }
+
+
+}
