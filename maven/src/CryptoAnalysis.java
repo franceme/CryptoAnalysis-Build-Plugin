ebd2644cf2626951ed8475834edb90d173565dfa
Melanie Bruns <mbruns42@mail.uni-paderborn.de>
2018-03-22 11:18:56 +0100

    Get log for cryptoanalysis and necessary maven dependencies for plugins

--- maven/src/CryptoAnalysis.java
+++ maven/src/CryptoAnalysis.java
@@ -6,15 +6,17 @@ import org.apache.maven.plugins.annotations.Mojo;
 
 public class CryptoAnalysisMojo extends AbstractMojo {
 
+    private Log log = getLog();
+
     /**
      * Runs the CogniCrypt static analysis to
      * check used security for potential security problems.
      *
      */
-    @Mojo( name = "CryptoAnalysis")
+    @Mojo( name = "cryptoanalysis")
     public void execute() throws MojoExecutionException
     {
-
+        log.info("CryptoAnalysis plugin started");
     }
 }
 
