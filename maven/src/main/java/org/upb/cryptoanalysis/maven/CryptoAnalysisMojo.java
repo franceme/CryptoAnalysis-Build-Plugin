dbfa8d7fa4c2b991c430225154fb806793b81447
Melanie Bruns <mbruns42@mail.uni-paderborn.de>
2018-03-27 14:21:24 +0200

    Add default phase and goal to mojo and get parameters from maven

--- maven/src/main/java/org/upb/cryptoanalysis/maven/CryptoAnalysisMojo.java
+++ maven/src/main/java/org/upb/cryptoanalysis/maven/CryptoAnalysisMojo.java
@@ -1,23 +1,44 @@
 package org.upb.cryptoanalysis.maven;
 
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import java.io.File;
 
 /**
  * Runs the CogniCrypt static analysis to
  * check used security for potential security problems.
  *
+ * @goal cyptoanalysis
+ * @phase verify
  */
 @Mojo( name = "cryptoanalysis")
 public class CryptoAnalysisMojo extends AbstractMojo {
 
+    @Parameter(defaultValue = "${project}", readonly = true)
+    private MavenProject project;
+    @Parameter(defaultValue = "${session}", readonly = true)
+    private MavenSession session;
+
+    @Parameter(property = "check.rulesDirectory")
+    private String rulesDirectory;
+
+    @Parameter(property = "check.callGraph", defaultValue = "CHA")
+    private String callGraph;
+
+    @Parameter(property = "check.reportsDirectory", defaultValue = "cognicrypt-reports")
+    private String reportsFolderParameter;
+    private File reportsFolder;
+
     private Log log = getLog();
 
-    public void execute() throws MojoExecutionException
-    {
+    public void execute() {
         log.info("CryptoAnalysis plugin started");
+        //TODO Pass information to analysis
     }
 }
 
